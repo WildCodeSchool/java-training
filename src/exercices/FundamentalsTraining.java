344624bc91b560d6eb1505f3c6fe02bdaf9517c6
bastienwcs <email>
2019-10-07 19:54:25 +0200

    add fundamentals training

---
+++ src/exercices/FundamentalsTraining.java
@@ -0,0 +1,170 @@
+public class FundamentalsTraining {
+
+    /**
+     * @param value, eg: 2
+     * @return the same value, eg: 2
+     */
+    public static int sameValue(int value) {
+        return 0;
+    }
+
+    /**
+     * @param value, eg: 2
+     * @return one added to the value, eg: 3
+     */
+    public static int addOne(int value) {
+        return 0;
+    }
+
+    /**
+     * @param first, eg: 2
+     * @param second, eg: 3
+     * @return first added to second, eg: 5
+     */
+    public static int addValues(int first, int second) {
+        return 0;
+    }
+
+    /**
+     * @param value, eg: 11
+     * @return true if value superior to 10, eg: true
+     */
+    public static boolean isValueSuperiorToTen(int value) {
+        return false;
+    }
+
+    /**
+     * @param value, eg: -1
+     * @return true if value inferior or equals to 0, eg: true
+     */
+    public static boolean isValueZeroOrNegative(int value) {
+        return false;
+    }
+
+    /**
+     * @param first, eg: 1
+     * @param second, eg: 1
+     * @return true if first equals second, eg: true
+     */
+    public static boolean areValuesEquals(int first, int second) {
+        return false;
+    }
+
+    /**
+     * @param first, eg: 2
+     * @param second, eg: 1
+     * @return true if first is superior to second, eg: true
+     */
+    public static boolean isFirstSuperiorToSecond(int first, int second) {
+        return false;
+    }
+
+    /**
+     * @param value, eg: 2
+     * @return true if value is even, eg: true
+     */
+    public static boolean isValueEven(int value) {
+        return false;
+    }
+
+    /**
+     * @param value, eg: 3
+     * @return true if value is odd, eg: true
+     */
+    public static boolean isValueOdd(int value) {
+        return false;
+    }
+
+    /**
+     * @param value, eg: 6
+     * @param divider, eg: 3
+     * @return true if value can be divided by divider, eg: true
+     */
+    public static boolean canValueBeDivided(int value, int divider) {
+        return false;
+    }
+
+    /**
+     * @param value, eg: true
+     * @return the string "true" if value is true, the string "false" otherwise,
+     * eg: "true"
+     */
+    public static String trueOrFalse(boolean value) {
+        return null;
+    }
+
+    /**
+     * @param value, eg: 1
+     * @return the string "one" if value is 1, the string "zero" if value is 0,
+     *  the string "other" otherwise, eg: "one"
+     */
+    public static String zeroOrOneOrOther(int value) {
+        return null;
+    }
+
+    /**
+     * @param first, eg: 1
+     * @param second, eg: 1
+     * @return the string "inferior" if first is inferior to second,
+     * the string "superior" if first is superior to second,
+     * the string "equals" otherwise, eg: "equals"
+     */
+    public static String inferiorOrSuperiorOrEquals(int first, int second) {
+        return null;
+    }
+
+    /**
+     * @param first, eg: 4
+     * @param second, eg: 3
+     * @return first plus second if first inferior to second,
+     * first minus second if first superior or equals to second, eg: 1
+     */
+    public static int addIfInferiorSubstractIfSuperiorOrEquals(int first, int second) {
+        return 0;
+    }
+
+    /**
+     * @param first, eg: true
+     * @param second, eg: false
+     * @return true if first or second value is true, eg: true
+     */
+    public static boolean firstOrSecond(boolean first, boolean second) {
+        return false;
+    }
+
+    /**
+     * @param first, eg: true
+     * @param second, eg: false
+     * @return true if first and second value is true, eg: false
+     */
+    public static boolean firstAndSecond(boolean first, boolean second) {
+        return false;
+    }
+
+    /**
+     * @param first, eg: false
+     * @param second, eg: false
+     * @return true if first and second value are equals, eg: true
+     */
+    public static boolean booleanEquals(boolean first, boolean second) {
+        return false;
+    }
+
+    /**
+     * @param value, eg: true
+     * @return false if value is true, true otherwise, eg: false
+     */
+    public static boolean notValue(boolean value) {
+        return false;
+    }
+
+    /**
+     * @param first, eg: 4
+     * @param second, eg: 3
+     * @param isNotFirst, eg: false
+     * @return first value if isNotFirst is false, second value otherwise, eg: 4
+     */
+    public static int whichValueIsThis(int first, int second, boolean isNotFirst) {
+        return 0;
+    }
+}
---
+++ src/tests/FundamentalsTrainingTest.java
@@ -0,0 +1,139 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FundamentalsTrainingTest extends MainTest {
+
+    @Test
+    public void sameValue() {
+        Assert.assertEquals(0, FundamentalsTraining.sameValue(0));
+        Assert.assertEquals(1, FundamentalsTraining.sameValue(1));
+        Assert.assertEquals(2, FundamentalsTraining.sameValue(2));
+    }
+
+    @Test
+    public void addOne() {
+        Assert.assertEquals(1, FundamentalsTraining.addOne(0));
+        Assert.assertEquals(2, FundamentalsTraining.addOne(1));
+        Assert.assertEquals(3, FundamentalsTraining.addOne(2));
+    }
+
+    @Test
+    public void addValues() {
+        Assert.assertEquals(0, FundamentalsTraining.addValues(0, 0));
+        Assert.assertEquals(1, FundamentalsTraining.addValues(0, 1));
+        Assert.assertEquals(4, FundamentalsTraining.addValues(2, 2));
+    }
+
+    @Test
+    public void isValueSuperiorToTen() {
+        Assert.assertFalse(FundamentalsTraining.isValueSuperiorToTen(0));
+        Assert.assertFalse(FundamentalsTraining.isValueSuperiorToTen(10));
+        Assert.assertTrue(FundamentalsTraining.isValueSuperiorToTen(11));
+    }
+
+    @Test
+    public void isValueZeroOrNegative() {
+        Assert.assertTrue(FundamentalsTraining.isValueZeroOrNegative(0));
+        Assert.assertTrue(FundamentalsTraining.isValueZeroOrNegative(-1));
+        Assert.assertFalse(FundamentalsTraining.isValueZeroOrNegative(1));
+    }
+
+    @Test
+    public void areValuesEquals() {
+        Assert.assertTrue(FundamentalsTraining.areValuesEquals(0, 0));
+        Assert.assertTrue(FundamentalsTraining.areValuesEquals(1, 1));
+        Assert.assertFalse(FundamentalsTraining.areValuesEquals(0, 1));
+    }
+
+    @Test
+    public void isFirstSuperiorToSecond() {
+        Assert.assertFalse(FundamentalsTraining.isFirstSuperiorToSecond(0, 0));
+        Assert.assertTrue(FundamentalsTraining.isFirstSuperiorToSecond(1, 0));
+        Assert.assertFalse(FundamentalsTraining.isFirstSuperiorToSecond(0, 1));
+    }
+
+    @Test
+    public void isValueEven() {
+        Assert.assertFalse(FundamentalsTraining.isValueEven(3));
+        Assert.assertTrue(FundamentalsTraining.isValueEven(2));
+        Assert.assertTrue(FundamentalsTraining.isValueEven(42));
+    }
+
+    @Test
+    public void isValueOdd() {
+        Assert.assertTrue(FundamentalsTraining.isValueEven(33));
+        Assert.assertTrue(FundamentalsTraining.isValueEven(3));
+        Assert.assertFalse(FundamentalsTraining.isValueEven(42));
+    }
+
+    @Test
+    public void canValueBeDivided() {
+        Assert.assertTrue(FundamentalsTraining.canValueBeDivided(4, 2));
+        Assert.assertTrue(FundamentalsTraining.canValueBeDivided(6, 3));
+        Assert.assertFalse(FundamentalsTraining.canValueBeDivided(11, 7));
+    }
+
+    @Test
+    public void trueOrFalse() {
+        Assert.assertEquals("true", FundamentalsTraining.trueOrFalse(true));
+        Assert.assertEquals("false", FundamentalsTraining.trueOrFalse(false));
+    }
+
+    @Test
+    public void zeroOrOneOrOther() {
+        Assert.assertEquals("zero", FundamentalsTraining.zeroOrOneOrOther(0));
+        Assert.assertEquals("one", FundamentalsTraining.zeroOrOneOrOther(1));
+        Assert.assertEquals("other", FundamentalsTraining.zeroOrOneOrOther(10));
+        Assert.assertEquals("other", FundamentalsTraining.zeroOrOneOrOther(-1));
+    }
+
+    @Test
+    public void inferiorOrSuperiorOrEquals() {
+        Assert.assertEquals("superior", FundamentalsTraining.inferiorOrSuperiorOrEquals(1, 0));
+        Assert.assertEquals("inferior", FundamentalsTraining.inferiorOrSuperiorOrEquals(0, 1));
+        Assert.assertEquals("equals", FundamentalsTraining.inferiorOrSuperiorOrEquals(0, 0));
+    }
+
+    @Test
+    public void addIfInferiorSubstractIfSuperiorOrEquals() {
+        Assert.assertEquals(3, FundamentalsTraining.addIfInferiorSubstractIfSuperiorOrEquals(1, 2));
+        Assert.assertEquals(1, FundamentalsTraining.addIfInferiorSubstractIfSuperiorOrEquals(2, 1));
+        Assert.assertEquals(0, FundamentalsTraining.addIfInferiorSubstractIfSuperiorOrEquals(1, 1));
+    }
+
+    @Test
+    public void firstOrSecond() {
+        Assert.assertTrue(FundamentalsTraining.firstOrSecond(true, false));
+        Assert.assertTrue(FundamentalsTraining.firstOrSecond(false, true));
+        Assert.assertTrue(FundamentalsTraining.firstOrSecond(true, true));
+        Assert.assertFalse(FundamentalsTraining.firstOrSecond(false, false));
+    }
+
+    @Test
+    public void firstAndSecond() {
+        Assert.assertFalse(FundamentalsTraining.firstAndSecond(true, false));
+        Assert.assertFalse(FundamentalsTraining.firstAndSecond(false, true));
+        Assert.assertTrue(FundamentalsTraining.firstAndSecond(true, true));
+        Assert.assertFalse(FundamentalsTraining.firstAndSecond(false, false));
+    }
+
+    @Test
+    public void booleanEquals() {
+        Assert.assertFalse(FundamentalsTraining.booleanEquals(true, false));
+        Assert.assertFalse(FundamentalsTraining.booleanEquals(false, true));
+        Assert.assertTrue(FundamentalsTraining.booleanEquals(false, false));
+        Assert.assertTrue(FundamentalsTraining.booleanEquals(true, true));
+    }
+
+    @Test
+    public void notValue() {
+        Assert.assertFalse(FundamentalsTraining.notValue(true));
+        Assert.assertTrue(FundamentalsTraining.notValue(false));
+    }
+
+    @Test
+    public void whichValueIsThis() {
+        Assert.assertEquals(0, FundamentalsTraining.whichValueIsThis(0, 1, true));
+        Assert.assertEquals(1, FundamentalsTraining.whichValueIsThis(0, 1, false));
+    }
+}
