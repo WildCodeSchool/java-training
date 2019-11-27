cacf306505764a252a141dfbeeac73fe103182f1
Casey Heagerty <cheagerty@gmail.com>
2019-11-27 16:29:07 +0100

    quest 1

--- src/exercices/PrimitiveTraining.java
+++ src/exercices/PrimitiveTraining.java
@@ -2,17 +2,26 @@ public class PrimitiveTraining {
 
     // TODO 1 : Change the method 'integer' to return the expected result : int 42
     public static int integer() {
-        return 0;
+        return 42;
     }
 
     // TODO 2 : Create a method 'str', result expected : String "Hello"
-
+    public static String str() {
+    	return "Hello";
+    }
 
     // TODO 3 : Create a method 'real', result expected : double 3.14
-
+    public static double real() {
+    	return 3.14;
+    }
 
     // TODO 4 : Create a method 'bool', result expected : boolean false
-
+    public static boolean bool() {
+    	return false;
+    }
 
     // TODO 5 : Create a method 'nothing', no result expected
+    public static void nothing() {
+    	return;
+    }
 }
